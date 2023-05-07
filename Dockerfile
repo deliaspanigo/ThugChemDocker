9f341151a8524d1d7c213d5bb838fbf2761da97e
deliaspanigo <d.eliaspanigo@gmail.com>
2023-05-07 11:20:10 +0200

    Version Gamma

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 # FROM rocker/shiny:4.1.0
 FROM rocker/shiny:4.1.0
 RUN rm -rf /srv/shiny-server/*
+
 WORKDIR /srv/shiny-server/
 COPY ./app.R ./app.R
 
@@ -10,3 +11,4 @@ EXPOSE 3838
 # basic shiny functionality
 RUN R -q -e "install.packages(c('devtools'))"
 RUN R -q -e "devtools::install_github('deliaspanigo/ThugChemR')"
+# RUN R -q -e "shiny::runApp(appDir = './', host = '0.0.0.0', port = 3838)"
--- app.R
+++ app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(usethis)
 library(devtools)
 library(ThugChemR)
 
@@ -9,4 +10,14 @@ ui <- ThugChemR::ui_ThugChemPage()
 # Le esta pasando la funcion completa!
 server <- ThugChemR::server_ThugChemPage
 
+# Armamos la app...
 shiny::shinyApp(ui = ui, server = server)
+# the_app <- shiny::shinyApp(ui = ui, server = server)
+
+# La levantamos con detalles particulares de puerto por ejemplo
+# shiny::runApp(the_app, host = '0.0.0.0', port = 3838)
+
+# shiny::shinyApp(ui = ui, server = server)
+# shiny::runApp(appDir = my_dir, port = 3838)
+# Run the application 
+# shiny::shinyApp(ui = ui, server = server)
